6070a5efb3f39ed16b7953a97ef8cc9696cfe7a3
javierh-castro <javi2cc1@gmail.com>
2024-09-23 14:59:56 -0300

    feat: Add icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="hidden md:block bg-gray-800 text-white py-4 fixed bottom-0 left-0 right-0">
       <div className="container max-w-6xl mx-auto text-center">
-        <p>&copy; 2024 - Administrador de Productos</p>
+        <p>&copy; 2024  Javier Castro Developer</p>
       </div>
     </footer>
   );
