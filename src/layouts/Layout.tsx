eba0ca3c8af130e120aaf5226b79159f03ed3bd1
javierh-castro <javi2cc1@gmail.com>
2024-10-11 17:26:43 -0300

    feat: Style the header and adjust the main for card appearance

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,20 +1,23 @@
-import { Outlet } from "react-router-dom"
-import Footer from "../components/Footer"
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 function Layout() {
   return (
     <>
-    <header className=" bg-slate-800">
-        <div className="mx-auto max-w-6xl py-10">
-            <h1 className="text-4xl font-extrabold text-white">Administrador de Productos</h1>
+      <header className="bg-slate-800 fixed top-0 left-0 w-full z-50">
+        <div className="mx-auto max-w-6xl py-6 md:py-10">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold sm:font-extrabold text-white text-center">
+            Administrador de Productos
+          </h1>
         </div>
-    </header>
-    <main className=" mt-10 container max-w-6xl mx-auto px-4 py-8 bg-white shadow rounded">
-    <Outlet/>
-    </main>
-    <Footer/>
+      </header>
+
+      <main className="mt-[96px] md:mt-[128px] mx-2 md:mx-auto px-4 py-8 bg-white shadow-lg rounded-lg md:max-w-6xl md:px-8 md:py-12 md:shadow-xl">
+        <Outlet />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
